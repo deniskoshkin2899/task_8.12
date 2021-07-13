cc67a920c70d17579cfcbd3c397c324f8ce17c83
deniskoshkin2899 <84732627+deniskoshkin2899@users.noreply.github.com>
2021-07-13 14:55:14 +0700

    Create task10.js

---
+++ task10.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+
+if(num > 1000) return alert("Вводимое значение превышает 1000");
+
+{
+  for(let i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num > 1;
+}
+}
+
+console.log(isPrime(1000)) 
+
